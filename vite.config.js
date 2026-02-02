a5a876126fc7164659b55f4848e2cb502d611f6c
Saketh <vnaagsaketh8925@gmail.com>
2026-02-02 17:49:56 +0530

    added vite config and env for production

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,18 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(),tailwindcss()],
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:7777',
+        target: process.env.VITE_API_URL || 'http://localhost:7777',
         changeOrigin: true
       }
     }
   }
-});
+})
+
+
+
+
